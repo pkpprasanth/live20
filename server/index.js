ddc36b4f01a8b7bcc00a8dbeec76fca97d9cacf9
Prasanth <55478056+prasanth132910@users.noreply.github.com>
2021-02-01 10:11:56 +0530

    Update Port

--- server/index.js
+++ server/index.js
@@ -30,8 +30,6 @@ app.post("/api/sendMail", (req, res) => {
 
 })
 
-
-//use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
